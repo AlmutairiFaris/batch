c6715a5ac3fd9c35531a9c9ba17c9f6610b7fac1
Faris Almutairi <farsm@FARIS>
2022-03-08 20:25:47 +0300

    test

--- GitCommands/createNewRepoFromCMD.bat
+++
@@ -1,9 +0,0 @@
-:: create a new repository on the command line
-
-echo "# batch" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/AlmutairiFaris/batch.git
-git push -u origin main
