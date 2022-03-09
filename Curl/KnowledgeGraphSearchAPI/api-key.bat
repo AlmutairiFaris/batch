32d223d91a2a8ea0c4766fa6d6ec6097b70cb964
Faris Almutairi <99851502+AlmutairiFaris@users.noreply.github.com>
2022-03-10 00:26:23 +0300

    Add files via upload

---
+++ Curl/KnowledgeGraphSearchAPI/api-key.bat
@@ -0,0 +1,10 @@
+:: To get your own API, visit the following links:
+:: 	https://developers.google.com/knowledge-graph/prereqs
+::      Go to the following link (for the setup tool)
+::	https://console.developers.google.com/start/api?id=kgsearch.googleapis.com&credential=client_key
+::	Go to API restrictions and add Google Knowledge Search
+:: 	https://console.developers.google.com/
+:: Follw the instructions to get your API key
+:: Copy the API key, and paste it here. 
+
+set "apiKey=HERE"
