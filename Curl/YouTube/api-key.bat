9332d9d841f46f40f4b6897b3688c77472aa72d9
Faris Almutairi <99851502+AlmutairiFaris@users.noreply.github.com>
2022-03-10 14:57:59 +0300

    Delete Curl/YouTube directory

--- Curl/YouTube/api-key.bat
+++
@@ -1,7 +0,0 @@
-:: To get your own API, visit the following links:
-:: 	https://developers.google.com/youtube/v3/docs#calling-the-api
-:: 	https://console.developers.google.com/
-:: Follw the instructions to gey your API key
-:: Copy the API key, and paste it here. 
-
-set "apiKey=HERE"
