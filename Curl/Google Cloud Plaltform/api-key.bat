c694b0d3de9681817425d03d15ef6ba54ea52895
Faris Almutairi <99851502+AlmutairiFaris@users.noreply.github.com>
2022-03-10 15:15:29 +0300

    Update api-key.bat

--- Curl/Google Cloud Plaltform/api-key.bat
+++ Curl/Google Cloud Plaltform/api-key.bat
@@ -3,7 +3,7 @@
 ::      In the search bar, type the name of the API, and select it
 ::	enable the desired API
 ::      then, go to APIs and services, then credentionals, then API keys
-::      if don't have API key, create it.
+::      if you don't have API key, create it.
 ::      if you have API key, press on the pen to edit it, then go to API restrictions, and select the API you enabled, then save
 :: Finally:
 :: 	Copy the API key, and paste it here. 
