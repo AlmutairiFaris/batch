1407b01948d5f86e9720ad54864f65cce8dd5348
Faris Almutairi <99851502+AlmutairiFaris@users.noreply.github.com>
2022-03-10 18:35:18 +0300

    Update deepAI-text-to-image.bat

--- Curl/deepAI/deepAI-text-to-image.bat
+++ Curl/deepAI/deepAI-text-to-image.bat
@@ -5,11 +5,17 @@ set "URL=https://api.deepai.org/api/text2img"
 
 echo This tool takes text as input, and returns an image as output.
 echo.
+
+:: Take input from user %text%
 set /p "text=Enter Text: "
+set outputDir=text2image
+
+:: Create folders. This is where the image will be downloaded and saved.
+if not exist "%outputDir%" md "%outputDir%"
+if not exist "%outputDir%/%text%" md "%outputDir%/%text%"
 
 :: input(text) -----> output(temp.txt)
 curl -X POST "%URL%" -F "text=%text%" -H "Api-Key: %apiKey%" > temp.txt
-
 
 :: input(temp.txt) -----> output(%id%)
 FOR /F "tokens=1,2,3* delims=, " %%i in (temp.txt) do (
@@ -23,14 +29,7 @@ if  %%i equ "output_url": set url=%%j
 del temp.txt
 
 :: input(%id%, %url%) -----> output(image)
-set outputDir=text2image
-
-if not exist "%outputDir%" md "%outputDir%"
-if not exist "%outputDir%/%text%" md "%outputDir%/%text%"
-
-
 curl -o "%outputDir%/%text%/%id%.jpg" %url%
-
 
 echo.
 pause
